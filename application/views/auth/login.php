5273f656c072173272942915676f72c3c60d66b7
andialfian23 <andialfi90@gmail.com>
2024-01-07 13:19:17 +0700

    menambah icon page login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login Page</title>
-
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>images/logo/cptn.png">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
